16ff27d72df01eb496ce763fbb901aff1eaaa0b5
Rifqi <91248079+Rifqi45@users.noreply.github.com>
2022-09-26 17:36:27 +0700

    Add files via upload

---
+++ No1.java
@@ -0,0 +1,10 @@
+package sesi1;
+
+public class No1 {
+    public static void main(String[] args) {
+        System.out.println("Belajar Java");
+        System.out.println("Sangat mudah sekali");
+        System.out.println("dan sangat menyenangkan");
+    }
+    
+}
---
+++ no2.java
@@ -0,0 +1,10 @@
+package sesi1;
+import javax.swing.JOptionPane;
+public class no2 {
+    public static void main(String[] args) {
+   String  belajar;
+   belajar  =   JOptionPane.showInputDialog("Anda sedang belajar apa?");
+   
+        System.out.println("belajar "+belajar+" sangat mudah");
+    }
+}
